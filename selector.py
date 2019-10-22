53b2a0ea9c0fd2eef3837e4f8dda5576db6f6901
hslyman <hslyman@ucdavis.edu>
2019-10-22 11:27:00 -0700

    still merging

--- selector.py
+++ selector.py
@@ -41,8 +41,10 @@ isolated = 0
 spliced = 0
 canonical = 0
 rna_supported = 0
-
 training = []
+
+o = open(arg.out, 'w+')
+o.write('region\tlen\ttxs\texons\trna_introns\tfit\n')
 
 for region in os.listdir(arg.regions):
 	prefix = arg.regions + '/' + region + '/' + region
@@ -58,37 +60,44 @@ for region in os.listdir(arg.regions):
 	spliced += 1
 	if meta['pc_issues']: continue
 	canonical += 1
-	#memorize RNA-seq supported intron coordinates
+	#memorize RNA-seq supported intron coordinates and count RNA-supported introns
 	gff = GFF_file(file=prefix + '.gff', source=arg.source)
 	rna_introns = gff.get(type='intron')
+	rna_count = 0
 	intron_support = {}
 	for intron in rna_introns:
 		if intron.source != 'RNASeq_splice': continue
+		rna_count += 1
 		if intron.beg not in intron_support:
 			intron_support[intron.beg] = {}
 			intron_support[intron.beg][intron.end] = True
 	#determine support for annotated introns
 	genome = Reader(gff=prefix + '.gff', fasta=prefix + '.fa', source=arg.source)
-	supported = 0
 	for chrom in genome:
+		supported = 0
+		tx_len = 0
+		ex_count = 0
 		genes = chrom.ftable.build_genes()
 		for tx in genes[0].transcripts():
+			if tx.end - tx.beg > tx_len:
+				tx_len = tx.end - tx.beg
+				ex_count = len(tx.exons)
 			for intron in tx.introns:
-				if intron.beg in intron_support and intron.end in intron_support[intron.beg]:
-					supported += 1
-	if meta['introns'] == supported:
-		rna_supported += 1 
-		training.append(region)
+				if (intron.beg in intron_support and
+					intron.end in intron_support[intron.beg]): supported += 1
+		if meta['introns'] == supported:
+			rna_supported += 1 
+			training.append(region)
+			glen = genes[0].end - genes[0].beg
+			tx_count = len(genes[0].transcripts())
+			o.write('{}\t{}\t{}\t{}\t{}\n'.format(region, glen, tx_count, ex_count,
+				rna_count))
+				
+o.close()
 
 r = open(arg.report, 'w+')
-r.write('coding:{}\nisolated:{}\nspliced:{}\ncanonical:{}\nsupported:{}'.format(coding, isolated, spliced, canonical, rna_supported))
+r.write('coding:{}\nisolated:{}\nspliced:{}\ncanonical:{}\nsupported:{}'.format(coding,
+	isolated, spliced, canonical, rna_supported))
 r.close()
 
-o = open(arg.out, 'w+')
-for region in training:
-	gff = arg.regions + '/' + region + '/' + region + '.gff'
-	fasta = arg.regions + '/' + region + '/' + region + '.fa'
-	genome = Reader(gff=gff, fasta=fasta, source=arg.source)
-	
-	
-o.close()
+
