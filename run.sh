5e84d8a4d504f74025acce5264fdd30fff58df39
Ian Korf <ifkorf@ucdavis.edu>
2019-10-31 10:07:14 -0700

    writing to each directory

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 setup.py --fasta c_elegans.PRJNA13758.WS270.genomic.fa --gff c_elegans.PRJNA13758.WS270.annotations.gff3 --out r2 --padding 100 --source wb.270
+python3 setup.py --fasta c_elegans.PRJNA13758.WS270.genomic.fa --gff c_elegans.PRJNA13758.WS270.annotations.gff3 --out region --padding 100 --source wb.270
 
-python3 selector.py --regions region --source wb.270 --report stats.txt --out table.txt
+python3 selector.py --regions region --source wb.270 --report stats.txt --out table.txt --isomax 50
