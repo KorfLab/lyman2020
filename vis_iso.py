95f0c514164ef90c2ec6b610731f21228b3751ef
Alex Kramer <amkramer2010@yahoo.com>
2019-11-22 01:45:09 -0800

    display and label isoforms in order of frequency (minimum of intron reads)

--- vis_iso.py
+++ vis_iso.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plot
 import matplotlib.patches as patches
+import matplotlib.figure as figure
 import math
+import random
 import argparse
+import numpy as np
 import os
 from grimoire.io import GFF_file
 
@@ -38,55 +41,71 @@ freq_target = arg.threshold + ' freq paths:'
 
 isoforms = []
 with open(ipath) as ifile:
-    start_intrs = False
-    mn = math.inf
-    mx = 0
-    for line in ifile:
-        line = line[:-1] # remove \n
-        if(start_intrs):
-            if('e-' in line):
-                break
-            introns = [x.split(',') for x in line.split(' ')]
-            for i in range(len(introns)):
-                introns[i] = [int(x) for x in introns[i]]
-            isoforms.append(introns)
-            first, last = introns[0][0], introns[len(introns) - 1][1]
-            mn = first if first < mn else mn
-            mx = last if last > mx else mx
-        if(line == freq_target):
-            start_intrs = True
+	gff = GFF_file(file=prefix + '.gff')
+	start_intrs = False
+	mn = math.inf
+	mx = 0
+	for line in ifile:
+		line = line[:-1] # remove \n
+		if(start_intrs):
+			if('e-' in line):
+				break
+			introns = [x.split(',') for x in line.split(' ')]
+			min_freq = math.inf
+			for i in range(len(introns)):
+				introns[i] = [int(x) for x in introns[i]]
+				results = gff.get(type='intron', beg=introns[i][0], end=introns[i][1])
+				results = [x for x in results if (x.beg == introns[i][0] and x.end == introns[i][1] and x.score != '.')]
+				curr_freq = int(float(results[0].score))
+				min_freq = curr_freq if curr_freq < min_freq else min_freq
+			isoforms.append((introns, min_freq))
+			first, last = introns[0][0], introns[len(introns) - 1][1]
+			mn = first if first < mn else mn
+			mx = last if last > mx else mx
+		if(line == freq_target):
+			start_intrs = True
 
-gff = GFF_file(file=prefix + '.gff')
 
+isoforms.sort(key=lambda x:x[1])
 ymax = len(isoforms) * (height + 5) + y_level
 fig, axis = plot.subplots(1)
 axis.set( ylim=(0, ymax))
 axis.set_yticklabels([])
 axis.set_yticks([])
+axis.set( xlim=(gff.get(type='exon', end=mn-1)[0].beg-pad, gff.get(type='exon', beg=mx+1)[0].end+pad))
+freq_x = plot.gca().get_xlim()[1] + 10
 
 for i in range(len(isoforms)):
-    introns = isoforms[i]
-    length = len(introns)
-    rects = []
-    boost = (height + 5) * i
-    start = gff.get(type='exon', end=introns[0][0]-1)[0]
-    end = gff.get(type='exon', beg=introns[length-1][1]+1)[0]
+	introns = isoforms[i][0]
+	freq = isoforms[i][1]
+	length = len(introns)
+	rects = []
+	boost = (height + 5) * i
+	start = gff.get(type='exon', end=introns[0][0]-1)[0]
+	end = gff.get(type='exon', beg=introns[length-1][1]+1)[0]
+	
+	rects.append(patches.Rectangle((start.beg, y_level + boost), start.end - start.beg, height, facecolor=color))
+	for j in range(len(introns)-1):
+		beg = introns[j][1]
+		rects.append(patches.Rectangle((beg, y_level + boost), introns[j+1][0] - beg, height, facecolor=color))
+	rects.append(patches.Rectangle((introns[length-1][1], y_level + boost), end.end - end.beg, height, facecolor=color))
+	for j in range(1, len(rects)):
+		left_x = rects[j-1].get_x() + rects[j-1].get_width()
+		left_y = rects[j-1].get_y() + rects[j-1].get_height()
+		right_x = rects[j].get_x()
+		right_y = rects[j].get_y() + rects[j].get_height()
+		mid_x = (right_x + left_x) / 2
+		mid_y = right_y + 2
+		axis.add_patch(rects[j-1])
+		plot.plot([left_x, mid_x],[left_y, mid_y],'k-', linewidth=0.5)
+		plot.plot([mid_x, right_x],[mid_y, right_y],'k-', linewidth=0.5)
+	last = rects[len(rects)-1]
+	axis.add_patch(last)
+	freq_y = last.get_y()
+	plot.text(freq_x, freq_y, str(isoforms[i][1]), fontsize=6)
+	plot.plot([last.get_x() + last.get_width(), freq_x], [last.get_y() + last.get_height()/2, last.get_y() + last.get_height()/2],
+	linestyle='dotted', color='grey')
 
-    rects.append(patches.Rectangle((start.beg, y_level + boost), start.end - start.beg, height, facecolor=color))
-    for j in range(len(introns)-1):
-        beg = introns[j][1]
-        rects.append(patches.Rectangle((beg, y_level + boost), introns[j+1][0] - beg, height, facecolor=color))
-    rects.append(patches.Rectangle((introns[length-1][1], y_level + boost), end.end - end.beg, height, facecolor=color))
-    for j in range(1, len(rects)):
-        left_x = rects[j-1].get_x() + rects[j-1].get_width()
-        left_y = rects[j-1].get_y() + rects[j-1].get_height()
-        right_x = rects[j].get_x()
-        right_y = rects[j].get_y() + rects[j].get_height()
-        mid_x = (right_x + left_x) / 2
-        mid_y = right_y + 2
-        axis.add_patch(rects[j-1])
-        plot.plot([left_x, mid_x],[left_y, mid_y],'k-', linewidth=0.5)
-        plot.plot([mid_x, right_x],[mid_y, right_y],'k-', linewidth=0.5)
-    axis.add_patch(rects[len(rects)-1])
+plot.show()
 
 plot.savefig(arg.out, dpi=400)
