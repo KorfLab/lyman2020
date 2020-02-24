42719887b3570266b17744a39850d45696ea27bb
Ian Korf <ifkorf@ucdavis.edu>
2020-02-24 11:10:50 -0800

    modified 27356 to 25571

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 # run setup and selector
 
 SIZE=`ls region | wc -w`
-if [ $SIZE != 27356 ]; then
-	python3 setup.py --fasta c_elegans.PRJNA13758.WS270.genomic.fa --gff c_elegans.PRJNA13758.WS270.annotations.gff3 --out region --padding 100 --source wb.270
-	python3 selector.py --regions region --source wb.270 --report stats.txt --out table.txt --isomax 50
+if [ $SIZE != 25571 ]; then
+	python3 setup.py --fasta c_elegans.PRJNA13758.WS274.genomic.fa --gff c_elegans.PRJNA13758.WS274.annotations.gff3 --out region --padding 100 --source wb.270
+	python3 selector.py --regions region --source wb.270 --report stats.txt --out table.txt --isomax 500
 fi
 
 # build testing and training sets (add conditional later)
@@ -23,5 +23,7 @@ for reg in `cut -f1 testing.txt`; do
 	cat region/$reg/$reg.gff >> testing.gff
 	cat region/$reg/$reg.fa >> testing.fa
 done
+
+for reg in `cat table.txt | awk '{if ($4 == 1) print}' | cut -f 1`; do python3 vis_iso.py --regions region --region $reg --threshold 1e-4 --txt region/$reg/$reg.iscount --out region/$reg/$reg.pdf; done
 
 # make figures or whatever
