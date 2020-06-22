10dc7e6857b10dc17ad75098d0e2d721a5ee1508
Ian Korf <ifkorf@ucdavis.edu>
2020-06-22 10:16:33 -0700

    refactor

--- splice_ga.py
+++ splice_ga.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
 
-# estimates splice site probabilities to arrive at the observed pattern
-# then generates splices at various depths
-
 import argparse
-import sys
 import random
-from grimoire.io import GFF_file
-from grimoire.sequence import DNA
-from grimoire.feature import Feature, mRNA, Gene, FeatureTable
+import sys
 from grimoire.genome import Reader
+from spliceomatic import isoformer
 
 parser = argparse.ArgumentParser(
-	description='Splice site estimator and generator.')
+	description='Splice site usage estimator')
 parser.add_argument('--fasta', required=True, type=str,
 	metavar='<path>', help='gene region, just one sequence')
 parser.add_argument('--gff', required=True, type=str,
@@ -29,59 +24,14 @@ parser.add_argument('--mut_rate', required=False, type=float, default=0.1,
 	metavar='<float>', help='number of breeders in population [%(default).3f]')
 parser.add_argument('--mut_mag', required=False, type=float, default=0.2,
 	metavar='<float>', help='number of breeders in population [%(default).3f]')
+parser.add_argument('--samples', required=False, type=int, default=1000,
+	metavar='<float>', help='transcripts in fitness calculation [%(default)i]')
 parser.add_argument('--seed', required=False, type=int,
 	metavar='<int>', help='set random seed')
+parser.add_argument('--verbose', action='store_true', help='show progress')
 arg = parser.parse_args()
 
 if arg.seed: random.seed(arg.seed)
-
-def isoformer(don, acc, pdon, pacc, samples=1000, min_intron=35, min_exon=15):
-
-	# generate transcripts
-	isoforms = {}
-	introns = {}
-	itotal = 0
-	for n in range(samples):
-		tds = [don[i] for i in range(len(don)) if random.random() < pdon[i]]
-		tas = [acc[i] for i in range(len(acc)) if random.random() < pacc[i]]
-		used = {}
-		splice = []
-		last_acc = 0
-		for d in tds:
-			edist = d - last_acc
-			if edist < min_exon: continue
-			for a in tas:
-				idist = a - d;
-				if idist >= min_intron and a not in used and d not in used:
-					intron = (d, a)
-					itotal += 1
-					if intron not in introns: introns[intron] = 1
-					else:                     introns[intron] += 1
-					splice.append(intron)
-					used[d] = True
-					used[a] = True
-					last_acc = a
-		
-		t = tuple(splice)
-		if t not in isoforms: isoforms[t] = 1
-		else:                 isoforms[t] += 1
-
-	for i in introns: introns[i] /= itotal
-
-	# get site usage
-	du, au = {}, {}
-	for iso in isoforms:
-		count = isoforms[iso]
-		for intron in iso:
-			d, a = intron
-			if d not in du: du[d] = count
-			else:           du[d] += count
-			if a not in au: au[a] = count
-			else:           au[a] += count
-	for v in du: du[v] /= samples
-	for v in au: au[v] /= samples
-
-	return isoforms, introns, du, au
 
 def distance(p1, p2):
 	sum = 0
@@ -90,8 +40,8 @@ def distance(p1, p2):
 		else:       sum += p1[i]
 	return sum
 
-def fitness(ind, don, acc, obs):
-	iso, iu, du, au = isoformer(don, acc, ind['pd'], ind['pa'])
+def fitness(ind, don, acc, obs, s):
+	iso, iu, du, au = isoformer(don, acc, ind['pd'], ind['pa'], samples=s)
 	d1 = distance(iu, obs)
 	d2 = distance(obs, iu)
 	return d1 + d2
@@ -165,6 +115,7 @@ gen = arg.generations
 par = arg.breeders
 mut = arg.mut_rate
 mag = arg.mut_mag
+sam = arg.samples
 
 ## intialize population
 ga = []
@@ -172,7 +123,7 @@ for i in range(pop):
 	pd = [random.random() for i in range(len(don))]
 	pa = [random.random() for i in range(len(acc))]
 	ind = {'pd':pd, 'pa':pa, 'fit':None}
-	ind['fit'] = fitness(ind, don, acc, obs)
+	ind['fit'] = fitness(ind, don, acc, obs, sam)
 	ga.append(ind)
 
 ## evolve population
@@ -180,29 +131,29 @@ for g in range(gen):
 	
 	# sort population by fitness (lower is better)
 	ga = sorted(ga, key=lambda k: k['fit'])
-	min, max, ave = ga_stats(ga, par)
-	print(g, min, ave, max)	
+	if arg.verbose:
+		min, max, ave = ga_stats(ga, par)
+		sys.stderr.write(f'{g} {min} {ave} {max}\n')	
 	
 	# replace less fit with children
 	for i in range(par, pop):
 		p1 = random.randint(0, par)
 		p2 = random.randint(0, par)
 		ga[i] = breed(ga[p1], ga[p2], mut, mag)
-		ga[i]['fit'] = fitness(ga[i], don, acc, obs)
+		ga[i]['fit'] = fitness(ga[i], don, acc, obs, sam)
 
 ga = sorted(ga, key=lambda k: k['fit'])
 best = ga[0]
 
-#############################
-# Part 3: generate isoforms #
-#############################
+##################
+# Part 3: output #
+##################
 
-iso, iu, du, au = isoformer(don, acc, best['pd'], best['pa'],
-	samples=arg.transcripts)
-print('isoforms:', len(iso), iso)
-print('introns:', len(iu), iu)
-print('donors:', len(du), du)
-print('acceptors:', len(au), au)
-print('estimated donor probs:', best['pd'])
-print('estimated acceptor probs:', best['pa'])
+print('Donors')
+for i in range(len(don)):
+	print(don[i], best['pd'][i])
+print('Acceptors')
+for i in range(len(acc)):
+	print(acc[i], best['pa'][i])
+
 
---
+++ spliceomatic.py
@@ -0,0 +1,51 @@
+import random
+
+def isoformer(don, acc, pdon, pacc, samples=1000, min_intron=35, min_exon=15):
+
+	# generate transcripts
+	isoforms = {}
+	introns = {}
+	itotal = 0
+	for n in range(samples):
+		tds = [don[i] for i in range(len(don)) if random.random() < pdon[i]]
+		tas = [acc[i] for i in range(len(acc)) if random.random() < pacc[i]]
+		used = {}
+		splice = []
+		last_acc = 0
+		for d in tds:
+			edist = d - last_acc
+			if edist < min_exon: continue
+			for a in tas:
+				idist = a - d;
+				if idist >= min_intron and a not in used and d not in used:
+					intron = (d, a)
+					itotal += 1
+					if intron not in introns: introns[intron] = 1
+					else:                     introns[intron] += 1
+					splice.append(intron)
+					used[d] = True
+					used[a] = True
+					last_acc = a
+		
+		t = tuple(splice)
+		if t not in isoforms: isoforms[t] = 1
+		else:                 isoforms[t] += 1
+
+	for i in introns: introns[i] /= itotal
+
+	# get site usage
+	du, au = {}, {}
+	for iso in isoforms:
+		count = isoforms[iso]
+		for intron in iso:
+			d, a = intron
+			if d not in du: du[d] = count
+			else:           du[d] += count
+			if a not in au: au[a] = count
+			else:           au[a] += count
+	for v in du: du[v] /= samples
+	for v in au: au[v] /= samples
+
+	return isoforms, introns, du, au
+
+
